d1e4a34646e9b3a51fab59b99e25b7f118e2949d
Txxxthless <lepash85@gmail.com>
2023-06-19 15:32:28 +0300

    Client application created

---
+++ client-app/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, CoreModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
