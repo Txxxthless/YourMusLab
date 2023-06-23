8ed27408646387fccfe4cfca8f556d2843195edf
Txxxthless <lepash85@gmail.com>
2023-06-23 16:07:13 +0300

    Basic UI for account detail page

--- client-app/src/app/account/account-detail/account-detail.component.ts
+++ client-app/src/app/account/account-detail/account-detail.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/shared/models/user';
+import { AccountService } from 'src/app/shared/service/account.service';
 
 @Component({
   selector: 'app-account-detail',
   templateUrl: './account-detail.component.html',
-  styleUrls: ['./account-detail.component.css']
+  styleUrls: ['./account-detail.component.css'],
 })
-export class AccountDetailComponent {
+export class AccountDetailComponent implements OnInit {
+  currentUser?: User;
 
+  constructor(private accountService: AccountService, private router: Router) {}
+
+  ngOnInit(): void {
+    if (this.accountService.currentUser.value) {
+      this.currentUser = this.accountService.currentUser.value;
+    } else {
+      this.router.navigateByUrl('/account/login');
+    }
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
